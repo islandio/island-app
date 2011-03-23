fb16587f41b2befbc72f5b590229282aae305524
Sander Pick <sanderpick@gmail.com>
2011-03-23 12:15:11 -0700

    verify domain

--- public/javascripts/jquery.tweet.js
+++ public/javascripts/jquery.tweet.js
@@ -65,17 +65,31 @@
       }
     });
     // time to text
-    function relative_time(time_value) {
-      var parsed_date = Date.parse(time_value);
-      var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-      var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
-      if(delta < 60) return 'less than a minute ago';  
-      else if(delta < 120) return 'about a minute ago';
-      else if(delta < (45*60)) return (parseInt(delta / 60)).toString() + ' minutes ago';  
-      else if(delta < (90*60)) return 'about an hour ago';
-      else if(delta < (24*60*60)) return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';  
-      else if(delta < (48*60*60)) return '1 day ago';  
-      else return (parseInt(delta / 86400)).toString() + ' days ago';
+    function relative_time(ts) {
+      var parsed_date = Date.parse(ts)
+        , relative_to = (arguments.length > 1) ? arguments[1] : new Date()
+        , delta = parseInt((relative_to.getTime() - parsed_date) / 1000)
+      ;
+      if (delta < 5)
+        return 'just now';
+      else if (delta < 15)
+        return 'just a moment ago';
+      else if (delta < 30)
+        return 'just a few moments ago';
+      else if (delta < 60) 
+        return 'less than a minute ago';
+      else if (delta < 120) 
+        return 'about a minute ago';
+      else if (delta < (45 * 60)) 
+        return (parseInt(delta / 60)).toString() + ' minutes ago';
+      else if (delta < (90 * 60)) 
+        return 'about an hour ago';
+      else if (delta < (24 * 60 * 60)) 
+        return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
+      else if (delta < (2 * 24 * 60 * 60)) 
+        return 'about a day ago';
+      else
+        return (parseInt(delta / 86400)).toString() + ' days ago';
     }
     // make array for query
     if(typeof(s.username) == "string") s.username = [s.username];
