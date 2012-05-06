5a2aed94c22d5586c8cd72de6caf6a745c90dde9
Sander Pick <sanderpick@gmail.com>
2012-05-06 12:43:51 -0700

    or not

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd ~/node-service/current/node_modules/now/node_modules/node-proxy/
-make
+#cd ~/node-service/current/node_modules/now/node_modules/node-proxy/
+#make
 node app.js --port 80 --db 'mongo://islander:V[AMF?UV{b@10.112.1.168:27017/island'
