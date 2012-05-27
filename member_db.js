21e630e9d87836b440fc0b00cd6d9b18cb307744
Sander Pick <sanderpick@gmail.com>
2012-05-27 10:49:54 -0700

    search test2

--- member_db.js
+++ member_db.js
@@ -586,9 +586,8 @@ MemberDb.prototype.searchPosts = function (str, cb) {
         if (err) return cb(err);
         if (!posts || posts.length === 0)
           return cb(null, []);
+        console.log(posts);
         posts.sort(function (a, b) {
-          if(!a) console.log(a);
-          if(!b) console.log(b);
           return b.created - a.created;
         });
         Step(
