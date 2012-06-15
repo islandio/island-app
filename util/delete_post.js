bfc0489da6afd9d05533bd6314b9a73884bac945
Sander Pick <sanderpick@gmail.com>
2012-06-15 10:50:11 -0700

    added delete post script

---
+++ util/delete_post.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+var log = require('console').log;
+var mongodb = require('mongodb');
+var util = require('util'), error = util.error,
+    debug = util.debug, inspect = util.inspect;
+var Step = require('step');
+var ObjectID = require('mongodb').BSONPure.ObjectID;
+var _ = require('underscore');
+_.mixin(require('underscore.string'));
+
+var MemberDb = require('../member_db.js').MemberDb;
+
+var optimist = require('optimist');
+var argv = optimist
+    .demand('key')
+    .default('env', 'dev')
+    .default('db', 'mongo://localhost:27018/island')
+    .argv;
+
+var db = argv.env === 'dev' ?
+          'mongo://localhost:27018/island' :
+          'mongo://islander:V[AMF?UV{b@10.112.1.168:27017/island';
+
+function errCheck(err, op) {
+  if (err) {
+    error('Error during ' + op + ':\n' + err.stack);
+    process.exit(1);
+  };
+}
+
+// Connect to DB.
+var memberDb;
+var _id;
+var post;
+
+Step(
+  function () {
+    var next = this;
+    mongodb.connect(db, {
+                      server: { poolSize: 4 },
+                      db: { native_parser: false,
+                            reaperTimeout: 600000 },
+                    }, function (err, db) {
+      errCheck(err, 'connect(' + db + ')');
+      new MemberDb(db, { ensureIndexes: false }, next);
+    });
+  },
+  function (err, mDb) {
+    memberDb = mDb;
+    this();
+  },
+  // find post
+  function (err) {
+    memberDb.collections.post.findOne({ key: argv.key }, this);
+  },
+  // find post media
+  function (err, p) {
+    errCheck(err, 'finding post');
+    if (!p)
+      errCheck(new Error('not found'), 'could not find post');
+    _id = p._id;
+    post = p;
+    var next = this;
+    memberDb.collections.media.find({ post_id: _id })
+            .toArray(function (err, meds) {
+      errCheck(err, 'finding medias');
+      if (meds.length > 0) {
+        var _next = _.after(meds.length * 3, next);
+        _.each(meds, function (med) {
+          log('\nDeleted media: ' + inspect(med));
+          memberDb.collections.media.remove({ _id: med._id }, _next);
+          log('Deleted media\'s hits.');
+          memberDb.collections.hit.remove({ media_id: _id }, _next);
+          log('Deleted media\'s ratings.');
+          memberDb.collections.rating.remove({ media_id: _id }, _next);
+        });
+      } else next();
+    });
+  },
+  // delete all hits, views, ratings, comments, posts, medias
+  function (err) {
+    errCheck(err, 'removing post');
+    log('\nDeleted post: ' + inspect(post));
+    memberDb.collections.post.remove({ _id: _id }, this.parallel());
+    log('Deleted post\'s views.');
+    memberDb.collections.view.remove({ post_id: _id }, this.parallel());
+    log('Deleted post\'s comments.');
+    memberDb.collections.comment.remove({ post_id: _id }, this.parallel());
+  },
+  // Done.
+  function (err) {
+    errCheck(err, 'at end');
+    log('\nAll done!\n');
+    process.exit(0);
+  }
+);
