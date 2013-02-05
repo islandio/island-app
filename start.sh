951ef2d690518f019c8b1800b4617a4c7e46cc67
Sander Pick <sanderpick@gmail.com>
2013-02-04 22:05:42 -0800

    cleanup

--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-app.js --db 'mongodb://nodejitsu_sanderpick:as3nonkk9502pe1ugseg3mj9ev@ds043947.mongolab.com:43947/nodejitsu_sanderpick_nodejitsudb9750563292' --redis_host 'nodejitsudb2554783797.redis.irstack.com' --redis_pass 'f327cfe980c971946e80b8e975fbebb4'
