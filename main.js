015e8989d21ed5b23c1ea9045849e5b50336c908
Sander Pick <sanderpick@gmail.com>
2013-08-18 09:50:41 -0700

    use stylus in production too

--- main.js
+++ main.js
@@ -219,6 +219,7 @@ Step(
     // Production only
     else {
       app.use(express.favicon(app.get('ROOT_URI') + '/img/favicon.ico'));
+      app.use(stylus.middleware({src: __dirname + '/public'}));
       app.use(express.static(__dirname + '/public', {maxAge: 31557600000}));
       app.use(app.router);
       app.use(express.errorHandler());
