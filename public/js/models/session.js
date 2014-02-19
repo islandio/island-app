837f4b05138c84a8188b9459d59f6623d4b4a30d
Sander Pick <sanderpick@gmail.com>
2014-02-19 01:07:14 -0700

    tweak logos

--- public/js/models/session.js
+++ public/js/models/session.js
@@ -13,18 +13,18 @@ define([
           '6c+', '7a', '7a+', '7b', '7b+', '7c', '7c+', '8a', '8a+', '8b',
           '8b+', '8c', '8c+', '9a', '9a+', '9b', '9b+', '9c', '9c+'],
 
-    name: function () {
+    formatName: function () {
       return this.get('name') || new Date(this.get('date')).format('mm/dd/yy');
     },
 
-    duration: function (mins) {
+    formatActivityDuration: function (mins) {
       if (!mins) return '';
       var hrs = mins / 60;
       var units = hrs !== 1 ? 'hrs': 'hr';
       return hrs + '<span class="units"> ' + units + '</span>';
     },
 
-    performance: function (num) {
+    formatActivityPerformance: function (num) {
       var str;
       switch (num) {
         case -1: str = 'weak'; break;
@@ -34,7 +34,7 @@ define([
       return str ? 'felt ' + str: '';
     },
 
-    grade: function (num, feel) {
+    formatTickGrade: function (num, feel) {
       if (num === undefined) return;
       var str = '';
       switch (feel) {
@@ -43,7 +43,19 @@ define([
         case 1: str = ' (hard)'; break;
       }
       return this.grades[num] + str;
-    }
+    },
+
+    formatTickDetails: function (t) {
+      var parts = [];
+      if (t.first) parts.push('1st');
+      else if (t.firstf) parts.push('1st (f)');
+      if (t.tries === 1) parts.push('onsight');
+      else if (t.tries === 2) parts.push('flash');
+      else if (t.tries === 3) parts.push('2nd go');
+      else if (t.tries === 4) parts.push('3rd go');
+      else if (t.tries === 5) parts.push('redpoint');
+      return parts.join(', ');
+    },
 
   });
 });
