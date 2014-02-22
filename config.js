f84ce97ec923567d35b1d84579e73326a02aa5c9
Sander Pick <sanderpick@gmail.com>
2014-02-21 18:51:32 -0700

    remove private data

--- config.js
+++ config.js
@@ -6,10 +6,10 @@
 exports.get = function (env) {
   return env === 'production' ?
       {
-        MONGO_URI: 'mongodb://islander:isL4nDm0n6o@zoe.mongohq.com:10016/island',
-        REDIS_HOST: 'beardfish.redistogo.com',
-        REDIS_PASS: '8e79e951bd58df62a99fef22e32f6ede',
-        REDIS_PORT: 9806
+        MONGO_URI: 'URI',
+        REDIS_HOST: 'HOST',
+        REDIS_PASS: 'PASS',
+        REDIS_PORT: 'PORT'
       }:
       {
         MONGO_URI: 'mongodb://localhost:27017/island',
