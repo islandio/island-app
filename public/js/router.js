06c99380da3f0076779034601f972d99ff7bde47
Sander Pick <sanderpick@gmail.com>
2015-04-08 23:58:58 -0600

    fix static service route

--- public/js/router.js
+++ public/js/router.js
@@ -269,7 +269,7 @@ define([
         if (this.app.profile && this.app.profile.member
             && !this.app.profile.member.welcomed) {
           this.app.profile.member.welcomed = true;
-          this.renderWelcome('Thanks for signing up for our private beta!');
+          this.renderWelcome('Thanks for signing up!');
         }
       }, this));
     },
