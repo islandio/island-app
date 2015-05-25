42fae3f08329837c506a7a88d0da0a8c3ef95c9c
Sander Pick <sanderpick@gmail.com>
2015-05-25 10:32:48 -0700

    initial commit for media page

--- public/js/models/media.js
+++ public/js/models/media.js
@@ -9,28 +9,7 @@ define([
 ], function (_, Backbone, util) {
   return Backbone.Model.extend({
 
-    src: function () {
-      var link = this.get('video').link;
-      switch (link.type) {
-        case 'vimeo':
-          return 'https://player.vimeo.com/video/' + link.id + '?api=1';
-          break;
-        case 'youtube':
-          return '//www.youtube.com/embed/' + link.id;
-          break;
-        default:
-          return '';
-      }
-    },
-
-    date: function () {
-      var date = new Date(this.get('created'));
-      return date.format('mmm d');
-    },
-
-    views: function () {
-      return util.addCommas(this.get('vcnt') || 0);
-    },
+    
 
   });
 });
---
+++ public/js/views/rows/media.js
@@ -0,0 +1,87 @@
+/*
+ * Event Row view
+ */
+
+define([
+  'jQuery',
+  'Underscore',
+  'views/boiler/row',
+  'mps',
+  'rest',
+  'text!../../../templates/rows/event.html',
+  'views/rows/session',
+  'views/rows/tick',
+  'views/rows/post',
+  'views/rows/crag',
+  'views/rows/ascent'
+], function ($, _, Row, mps, rest, template, Session, Tick, Post, Crag,
+      Ascent) {
+  return Row.extend({
+
+    attributes: function () {
+      return _.defaults({class: 'event'},
+          Row.prototype.attributes.call(this));
+    },
+
+    initialize: function (options, app) {
+      this.app = app;
+      this.template = _.template(template);
+      Row.prototype.initialize.call(this, options);
+    },
+
+    render: function (single, prepend) {
+      Row.prototype.render.call(this, single, prepend);
+      this.$el.prev('.event-divider').show();
+
+      // Determine sub view type.
+      var Action;
+      switch (this.model.get('action_type')) {
+        case 'session': Action = Session; break;
+        case 'tick': Action = Tick; break;
+        case 'post': Action = Post; break;
+        case 'crag': Action = Crag; break;
+        case 'ascent': Action = Ascent; break;
+      }
+
+      // Render action as sub-view.
+      if (Action) {
+        var model = this.model.get('action');
+        model.event = this.model.get('data');
+        var params = {
+          parentView: this,
+          model: model
+        };
+        if (this.model.get('action_type') === 'tick') {
+          params.mapless = true;
+          params.inlineWeather = true;
+          params.inlineTime = true;
+        }
+        this.action = new Action(params, this.app).render(true);
+      }
+
+      return this;
+    },
+
+    setup: function () {
+
+      // For rendering tooltips
+      this.$('.tooltip').tooltipster({delay: 300, multiple: true});
+      Row.prototype.setup.call(this);
+    },
+
+    destroy: function () {
+      if (this.action) {
+        this.action.destroy();
+      }
+      Row.prototype.destroy.call(this);
+    },
+
+    _remove: function (cb) {
+      this.$el.slideUp('fast', _.bind(function () {
+        this.destroy();
+        cb();
+      }, this));
+    },
+
+  });
+});
