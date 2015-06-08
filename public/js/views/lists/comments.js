782c269b14e663b86bf508930c075c13f9681b92
Sander Pick <sanderpick@gmail.com>
2015-06-08 14:24:14 -0600

    show all button for sidebar

--- public/js/views/lists/comments.js
+++ public/js/views/lists/comments.js
@@ -20,26 +20,21 @@ define([
 
     initialize: function (app, options) {
       this.template = _.template(template);
-      this.collection = new Collection;
+      this.collection = new Collection();
       this.options = options;
       this.type = options.type;
       this.Row = Row;
 
-      // Call super init.
       List.prototype.initialize.call(this, app, options);
 
-      // Client-wide subscriptions
       this.subscriptions = [];
 
       if (!this.options.hangtenOnly) {
-
-        // Socket subscriptions
         _.bindAll(this, 'collect', '_remove');
         this.app.rpc.socket.on('comment.new', this.collect);
         this.app.rpc.socket.on('comment.removed', this._remove);
       }
 
-        // Reset the collection.
       var list = this.parentView.model.get('comments') || [];
       var cnt = this.parentView.model.get('comments_cnt') || 0;
       this.collection.older = cnt - list.length;
@@ -82,7 +77,6 @@ define([
         }
       }
 
-      // Render hangtens.
       this.hangtens = new Hangtens(this.app, {parentView: this});
 
       return List.prototype.setup.call(this);
@@ -94,25 +88,22 @@ define([
       return List.prototype.destroy.call(this);
     },
 
-    // Bind mouse events.
     events: {
       'click .comments-signin': 'signin',
       'click .comments-older': 'older'
     },
 
-    // Collect new data from socket events.
     collect: function (data) {
-      if (data.parent_id === this.parentView.model.id
-          && !this.collection.get(-1)) {
+      if (data.parent_id === this.parentView.model.id &&
+          !this.collection.get(-1)) {
         this.collection.push(data);
       }
     },
 
-    // remove a model
     _remove: function (data) {
       var index = -1;
       var view = _.find(this.views, function (v) {
-        ++index
+        ++index;
         return v.model.id === data.id;
       });
 
@@ -124,17 +115,6 @@ define([
       }
     },
 
-    //
-    // Optimistically writes a comment.
-    //
-    // This function assumes that a comment will successfully be created on the
-    // server. Based on that assumption we render it in the UI before the 
-    // success callback fires.
-    //
-    // When the success callback fires, we update the comment model id from the
-    // comment created on the server. If the error callback fires, we remove 
-    // the comment from the UI and notify the user (or retry).
-    //
     write: function (e) {
       if (e) e.preventDefault();
 
@@ -143,11 +123,9 @@ define([
       input.val(util.sanitize(input.val()));
       if (input.val().trim() === '') return;
 
-      // For server.
       var payload = form.serializeObject();
       payload.body = util.sanitize(payload.body);
 
-      // Mock comment.
       var data = {
         id: -1,
         author: this.app.profile.member,
@@ -155,14 +133,11 @@ define([
         created: new Date().toISOString()
       };
 
-      // Add the parent id.
       payload.parent_id = this.parentView.model.id;
 
-      // Optimistically add comment to page.
       this.collection.push(data);
       input.val('').keyup();
 
-      // Now save the comment to server.
       rest.post('/api/comments/' + this.type, payload,
           _.bind(function (err, data) {
         if (err) {
@@ -170,7 +145,6 @@ define([
           return console.log(err);
         }
 
-        // Update the comment id.
         var comment = this.collection.get(-1);
         comment.set('id', data.id);
         this.$('#-1').attr('id', data.id);
@@ -189,7 +163,6 @@ define([
       var limit = this.collection.older;
       this.collection.older = 0;
 
-      // Get the older comments.
       rest.post('/api/comments/list', {
         skip: this.collection.length,
         limit: limit,
@@ -197,7 +170,6 @@ define([
       }, _.bind(function (err, data) {
         if (err) return console.log(err);
 
-        // Update the collection.
         var ids = _.pluck(this.collection.models, 'id');
         this.collection.options.reverse = true;
         var i = 0;
@@ -209,7 +181,6 @@ define([
         }, this));
         this.collection.options.reverse = false;
 
-        // Hide the button.
         this.$('.comments-older.comment').hide();
       }, this));
 
@@ -217,8 +188,6 @@ define([
 
     signin: function (e) {
       e.preventDefault();
-
-      // Render the signin view.
       mps.publish('member/signin/open');
     }
 
--- public/js/views/lists/followees.js
+++ public/js/views/lists/followees.js
@@ -29,21 +29,31 @@ define([
       this.app.rpc.socket.on('follow.removed', this._remove);
 
       // Reset the collection.
-      this.collection.count = this.app.profile.content.followees.count;
-      this.collection.reset(this.app.profile.content.followees.items);
+      this.latestList = this.app.profile.content.followees;
+      this.collection.count = this.latestList.count;
+      this.collection.more = this.collection.count - this.latestList.items.length;
+      this.collection.reset(this.latestList.items);
     },
 
     setup: function () {
-
-      // Save refs.
       this.count = this.$('.sidebar-heading-cnt');
       this.tip = this.$('.sidebar-tip');
+      this.footer = this.$('.sidebar-more');
+      if (this.footer.length === 0) {
+        delete this.footer;
+      }
+
+      if (this.collection.count === this.collection.length) {
+        this.footer.hide();
+      }
 
       this.updateCount();
       return List.prototype.setup.call(this);
     },
 
-    events: {},
+    events: {
+      'click .sidebar-more': 'more'
+    },
 
     destroy: function () {
       this.app.rpc.socket.removeListener('follow.new', this.collect);
@@ -93,6 +103,32 @@ define([
         }
       }
       this.count.text('(' + this.collection.count + ')');
+    },
+
+    more: function (e) {
+      var limit = this.collection.more;
+      this.collection.more = 0;
+
+      rest.post('/api/subscriptions/list', {
+        skip: this.collection.length,
+        limit: limit,
+        query: this.latestList.query
+      }, _.bind(function (err, data) {
+        if (err) return console.log(err);
+
+        var ids = _.pluck(this.collection.models, 'id');
+        var i = 0;
+        this.collection.options.reverse = false;
+        _.each(data.subscriptions.items, _.bind(function (c) {
+          if (!_.contains(ids, c.id)) {
+            this.collection.push(c);
+            ++i;
+          }
+        }, this));
+        this.collection.options.reverse = true;
+
+        this.footer.hide();
+      }, this));
     },
 
   });
