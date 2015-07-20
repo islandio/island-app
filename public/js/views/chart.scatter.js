b43d9d30d818974033484877586d1c3112741620
Eyal Cohen <eyalcohen1@gmail.com>
2015-07-20 00:35:33 -0700

    start making it responsive

--- public/js/views/chart.scatter.js
+++ public/js/views/chart.scatter.js
@@ -25,7 +25,7 @@ define([
 
     fadeTime: 300,
     legend_dy: 40,
-    leftWidthFrac: 0.175,
+    sideWidth: 200,
     vertInnerPad: 20,
     scatterOpacity: .4,
     colors: {
@@ -74,55 +74,68 @@ define([
           options.immediate);
     },
 
+    resize: function() {
+      this.renderGraph();
+      this._updateGraph(this.d.ticks, this.d.gradeDomain, this.d.timeDomain,
+          { immediate: true} );
+    },
+
     // Create the static graph elements
     renderGraph: function() {
 
       var self = this;
 
       // Static graph setup
-      this.margin = {top: 60, right: 0, bottom: 20, left: 0};
+      this.margin = {top: 60, right: 0, bottom: 20, left: 20};
       this.width = this.$el.width() - this.margin.left - this.margin.right;
       this.height = this.$el.height() - this.margin.top - this.margin.bottom;
+      console.log(this.width, this.height);
 
       // Create the baseline SVG
-      var svg = d3.select(this.$el.get(0)).append('svg')
+      if (this.svg) {
+        this.svg.remove();
+      }
+
+      this.svg = d3.select(this.$el.get(0)).append('svg')
+
+      this.svg
           .attr('width', this.$el.width())
           .attr('height', this.$el.height())
 
       // Generate widths and heights for the container classes
-      this.lwidth = (this.$el.width() - this.margin.left - this.margin.right)
-          * this.leftWidthFrac;
+      this.lwidth = this.sideWidth;
       this.rwidth = this.lwidth;
       this.mwidth = (this.$el.width() - this.margin.left - this.margin.right)
-          * (1 - this.leftWidthFrac) - this.rwidth;
+          - this.lwidth - this.rwidth;
+      // responsive stuff
+      if (this.mwidth < 300) {
+        this.mwidth = 0;
+      }
+
       this.bheight = 40
       this.theight = this.$el.height() - this.margin.top - this.margin.bottom
           - this.vertInnerPad - this.bheight;
 
       // Generate the 4 quadrant container classes
-      this.ltSvg = svg.append('g')
+      this.ltSvg = this.svg.append('g')
+          .attr('class', 'lt-svg')
           .attr('transform', 'translate(' + this.margin.left + ','
               + this.margin.top + ')');
 
-      this.mtSvg = svg.append('g')
+      this.mtSvg = this.svg.append('g')
+          .attr('class', 'mt-svg')
           .attr('transform', 'translate(' + (this.margin.left + this.lwidth) + ','
               + this.margin.top + ')');
 
-      this.rtSvg = svg.append('g')
+      this.rtSvg = this.svg.append('g')
+          .attr('class', 'rt-svg')
           .attr('transform', 'translate(' + (this.margin.left + this.lwidth
               + this.mwidth) + ',' + this.margin.top + ')');
 
       var btop = this.margin.top + this.theight + this.vertInnerPad;
-      /*
-      this.lbSvg = svg.append('g')
-          .attr('transform', 'translate(' + this.margin.left + ','
-              + btop + ')');
 
-      this.rbSvg = svg.append('g')
-          .attr('transform', 'translate(' + (this.margin.left + this.lwidth) + ','
-              + btop + ')');
-      */
-      this.rbSvg = svg.append('g')
+      this.mbSvg = this.svg.append('g')
+          .attr('class', 'mb-svg')
           .attr('transform', 'translate(' + (this.margin.left + this.lwidth) + ','
               + btop + ')');
 
@@ -158,10 +171,10 @@ define([
           .style('font-size', '14px')
           .style('font-weight', 'bold')
 
-      this.mtSvg.append('text')
+      this.title = this.mtSvg.append('text')
           .attr('x', this.mwidth/2)
           .attr('y', -5)
-          .text('Ascent Timeline')
+          .text('Timeline')
           .style('text-anchor', 'middle')
           .style('font-size', '14px')
           .style('font-weight', 'bold')
@@ -213,79 +226,80 @@ define([
 
       // Slider
 
-      this.slider = this.rbSvg.append('g')
-          .attr('class', 'slider')
+      if (this.mwidth !== 0) {
+        this.slider = this.mbSvg.append('g')
+            .attr('class', 'slider')
 
-      this.slider.on('mousedown', function() {
-        d3.event.preventDefault();
-        d3.event.stopPropagation();
-        var pos = d3.mouse(this);
-        var sl = sCenter(self.sliderLeft);
-        var sr = sCenter(self.sliderRight);
-        if (pos[0] > (sr + sl) / 2) {
-          var newPos = sr + (self.mwidth/12 * (pos[0] > sr ? 1 : -1));
-          self.updateRightSlider(newPos, false);
-        } else {
-          var newPos = sl + (self.mwidth/12 * (pos[0] > sl ? 1 : -1));
-          self.updateLeftSlider(newPos, false);
-        }
-      });
+        this.slider.on('mousedown', function() {
+          d3.event.preventDefault();
+          d3.event.stopPropagation();
+          var pos = d3.mouse(this);
+          var sl = sCenter(self.sliderLeft);
+          var sr = sCenter(self.sliderRight);
+          if (pos[0] > (sr + sl) / 2) {
+            var newPos = sr + (self.mwidth/12 * (pos[0] > sr ? 1 : -1));
+            self.updateRightSlider(newPos, false);
+          } else {
+            var newPos = sl + (self.mwidth/12 * (pos[0] > sl ? 1 : -1));
+            self.updateLeftSlider(newPos, false);
+          }
+        });
 
-      this.sliderBar = this.slider.append('rect')
-          .attr('class', 'slider-bar')
-          .attr('rx', 6)
-          .attr('width', this.mwidth)
-          .attr('height', 10)
-          .style('fill', 'grey')
+        this.sliderBar = this.slider.append('rect')
+            .attr('class', 'slider-bar')
+            .attr('rx', 6)
+            .attr('width', this.mwidth)
+            .attr('height', 10)
+            .style('fill', 'grey')
 
-      this.sliderHighlight = this.slider.append('rect')
-          .attr('class', 'slider-highlight')
-          .attr('width', this.mwidth)
-          .attr('height', this.sliderBar.attr('height'))
-          .style('fill', this.colors.redpoint)
+        this.sliderHighlight = this.slider.append('rect')
+            .attr('class', 'slider-highlight')
+            .attr('width', this.mwidth)
+            .attr('height', this.sliderBar.attr('height'))
+            .style('fill', this.colors.redpoint)
 
-      this.sliderLeft = this.slider.append('rect')
-          .attr('class', 'slider-left')
-          .attr('y', -this.sliderBar.attr('height')/2)
-          .attr('x', -5)
-          .attr('width', 10)
-          .attr('height', this.sliderBar.attr('height')*2)
-          .attr('rx', 2)
-          .attr('ry', 2)
-          .style('fill', '#333')
-          .style('cursor', 'pointer')
-          .on('mousedown', function(d) {
-            self.startMove('left')
-          })
+        this.sliderLeft = this.slider.append('rect')
+            .attr('class', 'slider-left')
+            .attr('y', -this.sliderBar.attr('height')/2)
+            .attr('x', -5)
+            .attr('width', 10)
+            .attr('height', this.sliderBar.attr('height')*2)
+            .attr('rx', 2)
+            .attr('ry', 2)
+            .style('fill', '#333')
+            .style('cursor', 'pointer')
+            .on('mousedown', function(d) {
+              self.startMove('left')
+            })
 
-      this.sliderRight = this.slider.append('rect')
-          .attr('class', 'slider-right')
-          .attr('y', -this.sliderBar.attr('height')/2)
-          .attr('x', this.mwidth-5)
-          .attr('width', 10)
-          .attr('height', this.sliderBar.attr('height')*2)
-          .attr('rx', 2)
-          .attr('ry', 2)
-          .style('fill', '#333')
-          .style('cursor', 'pointer')
-          .on('mousedown', function(d) {
-            self.startMove('right')
-          })
+        this.sliderRight = this.slider.append('rect')
+            .attr('class', 'slider-right')
+            .attr('y', -this.sliderBar.attr('height')/2)
+            .attr('x', this.mwidth-5)
+            .attr('width', 10)
+            .attr('height', this.sliderBar.attr('height')*2)
+            .attr('rx', 2)
+            .attr('ry', 2)
+            .style('fill', '#333')
+            .style('cursor', 'pointer')
+            .on('mousedown', function(d) {
+              self.startMove('right')
+            })
 
-      d3.select('body')
-          .on('mousemove', function() {
-            d3.event.preventDefault();
-            d3.event.stopPropagation();
-            if (self.mouse.moving) {
-              if (self.mouse.which === 'right') {
-                self.updateRightSlider(d3.mouse(self.sliderRight.node())[0], false);
-              } else {
-                self.updateLeftSlider(d3.mouse(self.sliderLeft.node())[0], false);
+        d3.select('body')
+            .on('mousemove', function() {
+              d3.event.preventDefault();
+              d3.event.stopPropagation();
+              if (self.mouse.moving) {
+                if (self.mouse.which === 'right') {
+                  self.updateRightSlider(d3.mouse(self.sliderRight.node())[0], false);
+                } else {
+                  self.updateLeftSlider(d3.mouse(self.sliderLeft.node())[0], false);
+                }
               }
-            }
-          })
-          .on('mouseup', _.bind(self.endMove, self));
-
+            })
+            .on('mouseup', _.bind(self.endMove, self));
+      }
 
 
       // Create a line generator function
@@ -297,12 +311,14 @@ define([
       // Create the legend
       var legendEntries = this.rtSvg.append('g')
           .attr('class', 'legend')
-          .attr('transform', 'translate(30, 50)')
+          .attr('transform', 'translate(50, 50)')
           .selectAll('legendEntries')
           .data(d3.entries(this.colors))
           .enter()
           .append('g')
-          .attr('class', 'legend-entry')
+          .attr('class', function(d) {
+            return 'legend-entry fadeable ' + d.key
+          })
           .attr('transform', function(d, idx) {
             return 'translate(' + 0 + ',' + (idx*30) + ')';
           });
@@ -335,6 +351,7 @@ define([
           .attr('font-size', 12)
           .attr('x', 15)
           .attr('y', 4)
+          .style('cursor', 'default')
 
       // Create some radio buttons. All these do is raise an event on click
       // These could be HTML but fit nicely in the layout of the SVG
@@ -380,7 +397,7 @@ define([
 
       var radioButtons = this.rtSvg.append('g')
           .attr('class', 'svg-buttons')
-          .attr('transform', 'translate(30, 200)')
+          .attr('transform', 'translate(50, 200)')
           .selectAll('svg-button')
           .data(this.buttons)
           .enter()
@@ -547,12 +564,14 @@ define([
     }, 2),
 
     _resetSliders: function() {
-      this.sliderLeft.transition().duration(500)
-          .attr('x', -Number(this.sliderLeft.attr('width'))/2);
-      this.sliderRight.transition().duration(500)
-          .attr('x', this.mwidth - Number(this.sliderRight.attr('width'))/2);
-      this.sliderHighlight.transition().duration(500)
-          .attr('x', 0).attr('width', this.mwidth);
+      if (this.mwidth !== 0) {
+        this.sliderLeft.transition().duration(500)
+            .attr('x', -Number(this.sliderLeft.attr('width'))/2);
+        this.sliderRight.transition().duration(500)
+            .attr('x', this.mwidth - Number(this.sliderRight.attr('width'))/2);
+        this.sliderHighlight.transition().duration(500)
+            .attr('x', 0).attr('width', this.mwidth);
+      }
 
     },
 
@@ -620,8 +639,9 @@ define([
       }
       var bgd = this._updateBarGraphData(data, filt);
 
-      //this.x2.domain([0, d3.max(bgd, function(d) { return d.value.total })]);
-      var barGraph = this.ltSvg.select('.barGroup').selectAll('.bars')
+      var barGroup = this.ltSvg.select('.barGroup');
+
+      var barGraph = barGroup.selectAll('.bars')
           .data(bgd, function(d) { return d.key; })
 
       var barGroupEnter = barGraph
@@ -630,15 +650,15 @@ define([
           .attr('class', 'bars');
 
       barGroupEnter.append('rect')
-          .attr('class', 'bar-onsite onsite fadeable')
+          .attr('class', 'bar-onsite onsite')
           .attr('x', this.lwidth)
           .attr('width', 0)
       barGroupEnter.append('rect')
-          .attr('class', 'bar-flash flash fadeable')
+          .attr('class', 'bar-flash flash')
           .attr('x', this.lwidth)
           .attr('width', 0)
       barGroupEnter.append('rect')
-          .attr('class', 'bar-redpoint redpoint fadeable')
+          .attr('class', 'bar-redpoint redpoint')
           .attr('x', this.lwidth)
           .attr('width', 0)
 
@@ -694,6 +714,63 @@ define([
           .on('mouseenter', this.barTip.show)
           .on('mouseleave', this.barTip.hide)
 
+      barGroup.select('.barCount').remove();
+      var barCount = barGroup.append('g').attr('class', 'barCount')
+
+      var barY
+      var count = -1;
+      var leftX = Number.MAX_VALUE;
+      d3.selectAll('.bars').each(function(d) {
+        var x = self.lwidth
+            - self.x2(d.value.redpoint + d.value.flash + d.value.onsite)
+        if (x < leftX) {
+          leftX = Number(x)
+          count = d.value.total;
+          // get y value in group transform
+          barY = Number(d3.select(this).attr('transform').split(',')[1].slice(0,-1));
+        }
+      });
+
+      if (count != -1) {
+        barCount
+            .append('circle')
+            .attr('cx', leftX)
+            .attr('cy', barY + this.y.rangeBand()/2)
+            .attr('r', 3)
+            .style('fill', '#333')
+            .style('opacity', 0)
+            .transition().delay(immediate ? 0: this.fadeTime*2)
+            .style('opacity', 1)
+
+        barCount
+            .append('line')
+            .attr('x1', leftX)
+            .attr('x2', leftX)
+            .attr('y1', barY + this.y.rangeBand()/2)
+            .attr('y2', barY + this.y.rangeBand()/2)
+            .style('stroke', 'black')
+            .style('fill', 'none')
+            .style('stroke-width', 1)
+            .style('stroke-dasharray', ('4, 4'))
+            .style('stroke-opacity', 0)
+            .transition()
+            .delay(immediate ? 0: this.fadeTime*2)
+            .duration(300)
+            .ease('linear')
+            .style('stroke-opacity', 1)
+            .attr('y2', barY-60)
+
+        barCount
+            .append('text')
+            .attr('x', leftX)
+            .attr('y', barY-60-7)
+            .style('text-anchor', 'middle')
+            .style('opacity', 0)
+            .style('fill', '#333')
+            .transition().delay(immediate ? 0: this.fadeTime*2 + 300).duration(250)
+            .style('opacity', 1)
+            .text(count)
+        }
     },
 
     // pass data or the function will grab it from the scatter plot
@@ -749,37 +826,38 @@ define([
 
       // Slider ticks
       var years = d3.time.year.range(new Date(xDomain[0]), new Date(xDomain[1] + 1));
-      this.slider.select('.sliderTicks').remove();
-      var sliderTicks = this.slider.append('g').attr('class', 'sliderTicks');
-      var sbh = Number(this.sliderBar.attr('height'));
-      _.each(years, function (y, idx) {
-        if (idx != 0 && idx != years.length-1) {
-          sliderTicks.append('line')
-              .attr('x1', self.x(y))
-              .attr('x2', self.x(y))
-              .attr('y1', sbh/2)
-              .attr('y2', sbh*1.5)
-              .style('stroke', '#333')
-              .style('stroke-opacity', 0)
+      if (this.mwidth !== 0) {
+        this.slider.select('.sliderTicks').remove();
+        var sliderTicks = this.slider.append('g').attr('class', 'sliderTicks');
+        var sbh = Number(this.sliderBar.attr('height'));
+        _.each(years, function (y, idx) {
+          if (idx != 0 && idx != years.length-1) {
+            sliderTicks.append('line')
+                .attr('x1', self.x(y))
+                .attr('x2', self.x(y))
+                .attr('y1', sbh/2)
+                .attr('y2', sbh*1.5)
+                .style('stroke', '#333')
+                .style('stroke-opacity', 0)
+                .transition()
+                .delay(immediate ? 0 : self.fadeTime)
+                .duration(immediate ? 0 : self.fadeTime)
+                .style('stroke-opacity', 1)
+          }
+          sliderTicks.append('text')
+              .text(new Date(y).format('yyyy'))
+              .attr('x', self.x(y))
+              .attr('y', sbh+15)
+              .style('text-anchor', 'middle')
+              .style('fill', '#999')
+              .style('font-size', 10)
+              .style('opacity', 0)
               .transition()
               .delay(immediate ? 0 : self.fadeTime)
               .duration(immediate ? 0 : self.fadeTime)
-              .style('stroke-opacity', 1)
-        }
-        sliderTicks.append('text')
-            .text(new Date(y).format('yyyy'))
-            .attr('x', self.x(y))
-            .attr('y', sbh+15)
-            .style('text-anchor', 'middle')
-            .style('fill', '#999')
-            .style('font-size', 10)
-            .style('opacity', 0)
-            .transition()
-            .delay(immediate ? 0 : self.fadeTime)
-            .duration(immediate ? 0 : self.fadeTime)
-            .style('opacity', 1)
-
-      });
+              .style('opacity', 1)
+        });
+      }
 
 
       // Build bar graph
@@ -974,6 +1052,13 @@ define([
       if (tick.tries <= 1) { return 'onsite'; }
       else if (tick.tries > 1 && tick.tries <=2) { return 'flash'; }
       else { return 'redpoint'; }
+    },
+
+    setTitle: function(t) {
+      var time = 100;
+      this.title.transition().duration(time).style('opacity', 0)
+      this.title.transition().delay(time).text(t);
+      this.title.transition().duration(time).delay(time).style('opacity', 1)
     },
 
     empty: function () {
