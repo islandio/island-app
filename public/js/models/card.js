9470c7868b70a5c465ab643ac1a8488f56e397a5
Sander Pick <sanderpick@gmail.com>
2015-07-19 23:10:56 -0700

    fixes to _getEventsByAction

--- public/js/models/card.js
+++ public/js/models/card.js
@@ -20,60 +20,60 @@ define([
       this.prefs = options.prefs;
     },
 
-    ticksByGrade: function (type) {
-      var ticks = {};
-      _.each(this.get('ticks')[type], _.bind(function (t) {
-        if (t.grade !== undefined) {
-          var system = type === 'r' ? this.prefs.grades.route:
-              this.prefs.grades.boulder;
-          var grade = this.gradeConverter[type].indexes(t.grade, null, system);
-          if (!ticks[grade]) {
-            ticks[grade] = [];
-          }
-          ticks[grade].push(t);
-        } else {
-          var k = 'ungraded';
-          if (!ticks[k]) {
-            ticks[k] = [];
-          }
-          ticks[k].push(t);
-        }
-      }, this));
-      return ticks;
-    },
+    // ticksByGrade: function (type) {
+    //   var ticks = {};
+    //   _.each(this.get('ticks')[type], _.bind(function (t) {
+    //     if (t.grade !== undefined) {
+    //       var system = type === 'r' ? this.prefs.grades.route:
+    //           this.prefs.grades.boulder;
+    //       var grade = this.gradeConverter[type].indexes(t.grade, null, system);
+    //       if (!ticks[grade]) {
+    //         ticks[grade] = [];
+    //       }
+    //       ticks[grade].push(t);
+    //     } else {
+    //       var k = 'ungraded';
+    //       if (!ticks[k]) {
+    //         ticks[k] = [];
+    //       }
+    //       ticks[k].push(t);
+    //     }
+    //   }, this));
+    //   return ticks;
+    // },
 
-    formatActivityDuration: function (mins) {
-      if (!mins) return '';
-      var hrs = mins / 60;
-      var units = hrs !== 1 ? 'hrs': 'hr';
-      return hrs + '<span class="units"> ' + units + '</span>';
-    },
+    // formatActivityDuration: function (mins) {
+    //   if (!mins) return '';
+    //   var hrs = mins / 60;
+    //   var units = hrs !== 1 ? 'hrs': 'hr';
+    //   return hrs + '<span class="units"> ' + units + '</span>';
+    // },
 
-    formatActivityPerformance: function (num) {
-      var str;
-      switch (num) {
-        case -1: str = 'weak'; break;
-        case 0: str = 'good'; break;
-        case 1: str = 'strong'; break;
-      }
-      return str ? 'felt ' + str: '';
-    },
+    // formatActivityPerformance: function (num) {
+    //   var str;
+    //   switch (num) {
+    //     case -1: str = 'weak'; break;
+    //     case 0: str = 'good'; break;
+    //     case 1: str = 'strong'; break;
+    //   }
+    //   return str ? 'felt ' + str: '';
+    // },
 
-    formatTickDetails: function (t) {
-      var parts = [];
-      if (t.first) parts.push('FA');
-      else if (t.firstf) parts.push('FFA');
-      if (t.tries === 1) parts.push('onsight');
-      else if (t.tries === 2) parts.push('flash');
-      else if (t.tries === 3) parts.push('2nd go');
-      else if (t.tries === 4) parts.push('3rd go');
-      else if (!t.tries || t.tries === 5) parts.push('redpoint');
-      return parts.join(', ');
-    },
+    // formatTickDetails: function (t) {
+    //   var parts = [];
+    //   if (t.first) parts.push('FA');
+    //   else if (t.firstf) parts.push('FFA');
+    //   if (t.tries === 1) parts.push('onsight');
+    //   else if (t.tries === 2) parts.push('flash');
+    //   else if (t.tries === 3) parts.push('2nd go');
+    //   else if (t.tries === 4) parts.push('3rd go');
+    //   else if (!t.tries || t.tries === 5) parts.push('redpoint');
+    //   return parts.join(', ');
+    // },
 
-    count: function (n) {
-      return n !== 0 ? '~' + util.addCommas(n): 0;
-    },
+    // count: function (n) {
+    //   return n !== 0 ? '~' + util.addCommas(n): 0;
+    // },
 
   });
 });
