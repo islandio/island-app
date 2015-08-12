01b43d27cbc955bc0a35bab28a261687f7cf24d6
Eyal Cohen <eyalcohen1@gmail.com>
2015-08-11 23:54:24 -0700

    Some more functional tests

---
+++ test/server/service.js
@@ -0,0 +1,61 @@
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+var _ = require('underscore');
+var async = require('async');
+
+var url = 'localhost:8080';
+
+function route(r, q) {
+  var base = '/service/' + r;
+  if (!q) return base;
+  var first = true;
+  _.each(q, function(v, k) {
+    base += first ? '?' : '&';
+    first = false;
+    base += (k + '=' + v);
+  });
+  return base;
+}
+
+var cookies;
+
+describe('Service (not logged in)', function() {
+
+  it('static profiles', function(done) {
+    request(url)
+        .get(route('profile'))
+        .expect(200)
+        .end(function(err, res) {
+          res.body.should.be.empty();
+          done(err);
+        });
+  });
+
+  it('dash profile', function(done) {
+    request(url)
+        .get(route('dashboard', {actions: 'all'}))
+        .expect(200, done);
+  });
+
+  it('media profile', function(done) {
+    request(url)
+        .get(route('media'))
+        .expect(200, function(err, res) {
+          res.body.content.events.should.not.be.empty();
+          res.body.content.events.items.should.be.Array();
+          done(err);
+        });
+  });
+
+  it('ticks', function(done) {
+    request(url)
+        .get(route('ticks') + '/islandTest')
+        .expect(200, function(err, res) {
+          res.body.content.page.ticks.should.be.empty();
+          done(err);
+        });
+  });
+
+
+});
---
+++ test/server/setupteardown.js
@@ -0,0 +1,124 @@
+// Run before all unit tests that require a database
+
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+var _ = require('underscore');
+var async = require('async');
+
+
+var url = 'localhost:8080';
+var cookies;
+
+function getOrCreateMember(name, logout, cb) {
+  var profile = {
+    username: name,
+    password: name,
+    email: name + '@' + name + 'com'
+  };
+
+  request(url)
+      .get('/api/members/' + name)
+      .expect(200)
+      .end(function(err, res) {
+        if (!err && _.isEmpty(res.body)) {
+          console.log('creating user ' + name);
+          request(url)
+              .post('/api/members')
+              .send(profile)
+              .expect(200)
+              .end(function(err, res) {
+                if (err || !logout) return cb(err);
+                var req = request(url).get('/service/logout');
+                cookies = res.headers['set-cookie'].pop().split(';')[0];
+                req.cookies = cookies;
+                req.expect(200, cb);
+              });
+        } else {
+          if (logout) return cb();
+          console.log('logging in user ' + name);
+          request(url)
+              .post('/api/members/auth')
+              .send(profile)
+              .expect(200)
+              .end(function(err, res) {
+                cookies = res.headers['set-cookie'].pop().split(';')[0];
+                return cb();
+              })
+        }
+      });
+};
+
+function deleteMember(name, cb) {
+  var profile = {
+    username: name,
+    password: name
+  }
+  request(url)
+      .post('/api/members/auth')
+      .send(profile)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return cb(err);
+        console.log('deleting user ' + name);
+        var req = request(url).delete('/api/members/' + name);
+        req.cookies = res.headers['set-cookie'].pop().split(';')[0];
+        req.expect(200, cb);
+      });
+}
+
+function createCrag(name, cb) {
+  var profile = {
+    name: name,
+    // SF
+    location: {
+      latitude: 37.7833,
+      longitude: -122.4167
+    }
+  }
+  console.log('creating crag ' + name);
+  var req = request(url).post('/api/crags');
+  req.cookies = cookies;
+  req.send(profile)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return cb(err);
+        return cb(null, res);
+      });
+}
+
+function createAscent(name, type, grade, cragid, cb) {
+  var profile = {
+    name: name,
+    crag_id: cragid,
+    type: type,
+    grade: grade
+  }
+  console.log('creating ascent ' + name);
+  var req = request(url).post('/api/ascents');
+  req.cookies = cookies;
+  req.send(profile)
+    .expect(200)
+    .end(function(err, res) {
+        if (err) return cb(err);
+        res.crag_id = cragid;
+        return cb(null, res);
+    });
+}
+
+
+before('building a mini island database of users, ascents, crags', function(done) {
+  this.timeout(30000);
+  async.waterfall([
+    function(cb) { getOrCreateMember('islandTest', false, cb) },
+    function(cb) { createCrag('crag1', cb) },
+    function(res, cb) { createAscent('ascent1', 'b', 3, res.body._id, cb) }
+  ], done);
+});
+
+after('clearing database', function(done) {
+  async.parallel([
+    function(cb) { deleteMember('islandTest', cb) }
+  ], done);
+});
+
