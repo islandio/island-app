03778c938c66adf6cab55607791b34ceab1049a6
Eyal Cohen <eyalcohen1@gmail.com>
2015-09-23 16:14:25 -0700

    Test for ituil function

---
+++ test/server/util.js
@@ -0,0 +1,16 @@
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+var _ = require('underscore');
+
+var iutil = require('island-util');
+
+describe('Island Utils', function() {
+  it('atmentions function', function() {
+    var teststr = 'Test of @mentions for @users of different @names'
+    var res = iutil.atmentions(teststr);
+    res[0].should.equal('mentions');
+    res[1].should.equal('users');
+    res[2].should.equal('names');
+  })
+});
