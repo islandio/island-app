197a9656ccd06684a3c41611def1eef06b24ccb5
Sander Pick <sanderpick@gmail.com>
2018-01-29 16:21:07 -0800

    remove if in upload script

--- scripts/upload-static.sh
+++ scripts/upload-static.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-if [[ (! -f ./build/build.txt) ]]; then
-echo "ERROR: Run 'npm run build' first.\n"
-exit 1
-fi
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
 ./node_modules/.bin/s3-cli sync --delete-removed --config s3cmd.conf --acl-public ./build s3://builds-island/$PACKAGE_VERSION
